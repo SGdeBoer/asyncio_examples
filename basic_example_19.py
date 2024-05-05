70fcb20ddc69f7b35bbe89e77551b9b3c62f3fcb
Sam de Boer <samdeboer1997@hotmail.com>
2024-05-05 11:34:00 +0200

    removing an unnecessary layer from example19 decorator

--- basic_example_19.py
+++ basic_example_19.py
@@ -14,30 +14,28 @@ logger = logging.getLogger(__name__)
 logger.setLevel("INFO")
 
 
-def time_await():
-    def wrapper(func: Callable) -> Callable:
-        @functools.wraps(func)
-        async def wrapped(*args, **kwargs) -> Any:
-            logger.info(f"running {func} with args {args} {kwargs}")
-            start = timeit.default_timer()
-            try:
-                return await func(*args, **kwargs)
-            finally:
-                end = timeit.default_timer()
-                total = end - start
-                logger.info(f"finished {func} in {total}s")
-        return wrapped
-    return wrapper
+def time_await(func: Callable) -> Callable:
+    @functools.wraps(func)
+    async def wrapped(*args, **kwargs) -> Any:
+        logger.info(f"running {func} with args {args} {kwargs}")
+        start = timeit.default_timer()
+        try:
+            return await func(*args, **kwargs)
+        finally:
+            end = timeit.default_timer()
+            total = end - start
+            logger.info(f"finished {func} in {total}s")
+    return wrapped
 
 
-@time_await()
+@time_await
 async def dummy_operation():
     logger.info("starting dummy operation")
     await asyncio.sleep(3)
     logger.info("finished dummy operation")
 
 
-@time_await()
+@time_await
 async def main():
     await asyncio.gather(dummy_operation(), dummy_operation())
 
