6483ae3c5942f57f9e43d5062055088274a4323e
Sam de Boer <samdeboer1997@hotmail.com>
2024-05-09 17:29:57 +0200

    adding a new async example with run_until_complete

---
+++ basic_example_20.py
@@ -0,0 +1,39 @@
+"""
+Example showcasing why using run_until_complete to avoid marking functions as async is not good practice
+"""
+
+import asyncio
+import timeit
+import logging
+from typing import Callable, Any
+
+FORMAT = '%(asctime)s %(message)s'
+logging.basicConfig(format=FORMAT)
+logger = logging.getLogger(__name__)
+logger.setLevel("INFO")
+
+
+async def heavy_task():
+    logger.info("heavy task running")
+    await asyncio.sleep(10)
+
+
+async def light_task():
+    await asyncio.sleep(1)
+
+
+def main():
+    loop = asyncio.new_event_loop()
+
+    logger.info("scheduling heavy task")
+    task = loop.create_task(heavy_task())
+    logger.info("done scheduling heavy task")
+
+    logger.info("running light task till completion")
+    loop.run_until_complete(light_task())
+    logger.info("done running light task")
+
+
+if __name__ == "__main__":
+    main()
+    exit(0)
