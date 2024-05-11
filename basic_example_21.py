2a75faec7f2ca094d69e22e6d5202b994a52017c
Sam de Boer <samdeboer1997@hotmail.com>
2024-05-11 19:50:34 +0200

    adding a new async example with as_completed

---
+++ basic_example_21.py
@@ -0,0 +1,32 @@
+"""
+Example showcasing why using run_until_complete to avoid marking functions as async is not good practice
+"""
+
+import asyncio
+import random
+import logging
+
+FORMAT = '%(asctime)s %(message)s'
+logging.basicConfig(format=FORMAT)
+logger = logging.getLogger(__name__)
+logger.setLevel("INFO")
+
+
+async def random_sleep(sleep_time):
+    logger.info(f"running coro that sleeps for {sleep_time}")
+    await asyncio.sleep(sleep_time)
+    return sleep_time
+
+
+async def amain():
+    sleep_times = [random.random() * 5 for _ in range(5)]
+    logger.info(f"sleep times are {sleep_times}")
+    coros = [random_sleep(sleep_time) for sleep_time in sleep_times]
+
+    for coro in asyncio.as_completed(coros):
+        result = await coro
+        logger.info(f"ready to handle result of coro that slept for {result}")
+
+
+if __name__ == "__main__":
+    asyncio.run(amain())
